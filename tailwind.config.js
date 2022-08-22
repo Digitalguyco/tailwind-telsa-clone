801fcfb2950adcd48dd0359f2dbaf4b54e32111a
Ikekwem Daniel <87384972+Digitalguyco@users.noreply.github.com>
2022-08-22 18:26:56 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {  colors: {
+      "tesla-dark": "#393c41",
+    },},
+  },
+  plugins: [],
+}
