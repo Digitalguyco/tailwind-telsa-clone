62ff83586caef74bf1a74921cb5d272d4bc30585
Digitalguyco <youngkhito@gmail.com>
2022-08-22 10:15:29 -0700

    first push

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        "telsa-dark": "#393c41"
+      },
+    },
+  },
+  plugins: [],
+}
